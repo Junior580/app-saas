2b2d953205f4df750b6c28c6cce773c1586ea34a
jr <junior_silvapereira@hotmail.com>
2025-01-18 11:32:21 -0300

    initial permission config

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,11 +1,15 @@
 import {
+  AbilityBuilder,
   CreateAbility,
   createMongoAbility,
   ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
 
-const actions = ['manage', 'invite'] as const
+import { User } from './models/user'
+import { permissions } from './permissions'
+
+const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 type AppAbilities = [
   (typeof actions)[number],
@@ -17,3 +21,17 @@ type AppAbilities = [
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
