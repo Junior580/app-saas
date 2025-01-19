e0aefa966f40264bd8d300acb48bb216f821933d
jr <junior_silvapereira@hotmail.com>
2025-01-19 12:01:02 -0300

    feat: prisma initial config

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user_id' })
-
-const project = projectSchema.parse({ id: 'project_id', ownerId: 'user-id' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
