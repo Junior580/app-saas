9f7ee5242bf4756589bf1ae950a96f679f28b1f7
jr <junior_silvapereira@hotmail.com>
2025-02-26 20:58:00 -0300

    feat: get user info

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { auth } from '@/auth/auth'
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await auth()
   return (
     <>
       <div>
         <h1>Sign in</h1>
+        {JSON.stringify(user)}
         <Button>Sign in</Button>
       </div>
     </>
