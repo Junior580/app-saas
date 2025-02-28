45176225625746b7704f009748d6cfe4c6081ecf
jr <junior_silvapereira@hotmail.com>
2025-02-27 23:20:11 -0300

    feat: get ability

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export default function Header() {
+export default async function Header() {
+  const permissions = await ability()
+
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -19,7 +22,10 @@ export default function Header() {
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <span>Projetos</span>}
       </div>
+
       <div className="flex items-center gap-4">
         <ProfileButton />
       </div>
