0f3f51d0a541e5b029dafa0101c55e5adaf2a45e
jr <junior_silvapereira@hotmail.com>
2025-03-02 01:12:04 -0300

    feat: create org parallel route

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -2,9 +2,11 @@ import Header from '@/components/header'
 
 export default function Projects() {
   return (
-    <div className="py-4">
+    <div className="space-y-4 py-4">
       <Header />
-      <main></main>
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p className="text-sm text-muted-foreground">a</p>
+      </main>
     </div>
   )
 }
