496c44caddf30392f27015bba794eae48b9bb20f
jr <junior_silvapereira@hotmail.com>
2025-03-03 17:50:42 -0300

    feat: invite page and actions

--- apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -1,5 +1,5 @@
 import { organizationSchema } from '@saas/auth'
-import { ArrowLeftRight, Crown } from 'lucide-react'
+import { ArrowLeftRight, Crown, UserMinus } from 'lucide-react'
 import Image from 'next/image'
 
 import getCurrentOrg, { ability } from '@/auth/auth'
@@ -9,6 +9,9 @@ import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { getMembers } from '@/http/get-members'
 import { getMembership } from '@/http/get-membership'
 import { getOrganization } from '@/http/get-organization'
+
+import { removeMemberAction } from './actions'
+import { UpdateMemberRoleSelect } from './update-member-role-select'
 
 export async function MemberList() {
   const currentOrg = await getCurrentOrg()
@@ -72,6 +75,32 @@ export async function MemberList() {
                         <ArrowLeftRight className="mr-2 size-4" />
                         Transfer ownership
                       </Button>
+                    )}
+
+                    <UpdateMemberRoleSelect
+                      memberId={member.id}
+                      value={member.role}
+                      disabled={
+                        member.userId === membership.userId ||
+                        member.userId === organization.ownerId ||
+                        permissions?.cannot('update', 'User')
+                      }
+                    />
+
+                    {permissions?.can('delete', 'User') && (
+                      <form action={removeMemberAction.bind(null, member.id)}>
+                        <Button
+                          disabled={
+                            member.userId === membership.userId ||
+                            member.userId === organization.ownerId
+                          }
+                          size="sm"
+                          variant="destructive"
+                        >
+                          <UserMinus className="mr-2 size-4" />
+                          Remove
+                        </Button>
+                      </form>
                     )}
                   </div>
                 </TableCell>
--- apps/web/src/hooks/use-form-state.ts
+++ apps/web/src/hooks/use-form-state.ts
@@ -1,4 +1,5 @@
 import { FormEvent, useState, useTransition } from 'react'
+// import { requestFormReset } from 'react-dom'
 
 interface FormState {
   success: boolean
@@ -35,6 +36,7 @@ export function useFormState(
       }
 
       setFormState(state)
+      // requestFormReset(form)
     })
   }
 
