8b492e77fb93d9b796422be6435dad542bc93e86
jr <junior_silvapereira@hotmail.com>
2025-03-04 11:55:38 -0300

    fix: github url

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -4,8 +4,10 @@ import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
-  // const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
-  const githubSignInURL = new URL('https://github.com/login/oauth/authorize')
+  const githubSignInURL = new URL(
+    '/login/oauth/authorize',
+    'https://github.com',
+  )
 
   githubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignInURL.searchParams.set(
