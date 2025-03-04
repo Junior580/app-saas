110eb406c739eab488ddfeb99a5e68a9b55be0cb
jr <junior_silvapereira@hotmail.com>
2025-03-04 12:40:16 -0300

    fix: auth laoyut fix

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,53 +1,17 @@
-// import { redirect } from 'next/navigation'
-//
-// import { isAuthenticated } from '@/auth/auth'
-//
-// export default async function AppLayout({
-//   children,
-//   sheet,
-// }: Readonly<{
-//   children: React.ReactNode
-//   sheet: React.ReactNode
-// }>) {
-//   if (!(await isAuthenticated())) {
-//     redirect('/auth/sign-in')
-//   }
-//
-//   return (
-//     <>
-//       {children}
-//       {sheet}
-//     </>
-//   )
-// }
-'use client' // Marca o componente como cliente
-
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
 
-export default function AppLayout({
+export default async function AppLayout({
   children,
   sheet,
-}: {
+}: Readonly<{
   children: React.ReactNode
   sheet: React.ReactNode
-}) {
-  const router = useRouter()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    isAuthenticated().then((auth) => {
-      if (!auth) {
-        router.push('/auth/sign-in')
-      } else {
-        setLoading(false)
-      }
-    })
-  }, [])
-
-  if (loading) return <p>Loading...</p> // Ou um skeleton
+}>) {
+  if (!(await isAuthenticated())) {
+    redirect('/auth/sign-in')
+  }
 
   return (
     <>
